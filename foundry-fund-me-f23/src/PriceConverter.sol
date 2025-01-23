0ae7bb39e660f37933e8f138c2782d2481f23a14
Robert <mister.robert.weber@gmail.com>
2025-01-23 14:21:16 +0100

    adding FundMeTest

--- foundry-fund-me-f23/src/PriceConverter.sol
+++ foundry-fund-me-f23/src/PriceConverter.sol
@@ -1,27 +1,23 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity 0.8.28;
 
-import {AggregatorV3Interface} from "../lib/chainlink-brownie-contracts/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {AggregatorV3Interface} from "chainlink-brownie-contracts/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
-    function getPrice(
-        AggregatorV3Interface priceFeed
-    ) internal view returns (uint256) {
-        (, int256 answer, , , ) = priceFeed.latestRoundData();
-        // ETH/USD rate in 18 digit
-        return uint256(answer * 10000000000);
+    function getPrice() internal view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+
+        return uint256(price * 1e10);
     }
 
-    // 1000000000
-    // call it get fiatConversionRate, since it assumes something about decimals
-    // It wouldn't work for every aggregator
     function getConversionRate(
-        uint256 ethAmount,
-        AggregatorV3Interface priceFeed
+        uint256 ethAmount
     ) internal view returns (uint256) {
-        uint256 ethPrice = getPrice(priceFeed);
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
-        // the actual ETH/USD conversation rate, after adjusting the extra 0s.
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
         return ethAmountInUsd;
     }
 }
