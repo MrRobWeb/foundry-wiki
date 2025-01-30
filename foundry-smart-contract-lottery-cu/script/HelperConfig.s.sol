a8e8b6dc4ec4da39a2161a4a67d0dfce89e92478
Robert <mister.robert.weber@gmail.com>
2025-01-30 06:34:51 +0100

    adding script Interactions.s.sol with functions createSubscriptionId, fundSubscription

--- foundry-smart-contract-lottery-cu/script/HelperConfig.s.sol
+++ foundry-smart-contract-lottery-cu/script/HelperConfig.s.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 import {VRFCoordinatorV2_5Mock} from "lib/chainlink-brownie-contracts/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
+import {LinkToken} from "test/mocks/LinkToken.sol";
 
 abstract contract CodeConstants {
     // ** VRF Mock Values **
@@ -11,7 +12,7 @@ abstract contract CodeConstants {
     // Link / ETH price
     int256 public MOCK_WEI_PER_UNIT_LINK = 4e15;
 
-    uint256 public constant ETH_SEPOLIA_CHAIN_ID = 1115511;
+    uint256 public constant ETH_SEPOLIA_CHAIN_ID = 11155111;
     uint256 public constant LOCAL_CHAIN_ID = 31337;
 }
 
@@ -25,6 +26,7 @@ contract HelperConfig is CodeConstants, Script {
         bytes32 gasLane;
         uint32 callbackGasLimit;
         uint256 subscriptionId;
+        address linkToken;
     }
 
     NetworkConfig public localNetworkConfig;
@@ -62,6 +64,7 @@ contract HelperConfig is CodeConstants, Script {
             MOCK_GAS_PRICE_LINK,
             MOCK_WEI_PER_UNIT_LINK
         );
+        LinkToken linkToken = new LinkToken();
         vm.stopBroadcast();
 
         localNetworkConfig = NetworkConfig({
@@ -70,7 +73,8 @@ contract HelperConfig is CodeConstants, Script {
             vrfCoordinator: address(vrfCoordinatorMock),
             gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
             callbackGasLimit: 500000, // 500,000 gas
-            subscriptionId: 0 // might have to fix this
+            subscriptionId: 0, // might have to fix this
+            linkToken: address(linkToken)
         });
 
         return localNetworkConfig;
@@ -84,19 +88,8 @@ contract HelperConfig is CodeConstants, Script {
                 vrfCoordinator: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
                 gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
                 callbackGasLimit: 500000, // 500,000 gas
-                subscriptionId: 0
-            });
-    }
-
-    function getLocalConfig() public pure returns (NetworkConfig memory) {
-        return
-            NetworkConfig({
-                entranceFee: 0.01 ether,
-                interval: 30, // 30 seconds
-                vrfCoordinator: address(0),
-                gasLane: "",
-                callbackGasLimit: 500000,
-                subscriptionId: 0
+                subscriptionId: 69024070252057815200057885449354343369019403779240021056850182755373635971641,
+                linkToken: 0x779877A7B0D9E8603169DdbD7836e478b4624789
             });
     }
 }
